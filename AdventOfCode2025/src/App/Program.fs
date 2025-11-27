a4fe08d5c881581101dd2ffa7e9b40d11a232c13
Andreas Peter <andreas.peter.ch@gmail.com>
2025-11-27 22:17:43 +0100

    Example setup for AoC with 2024 example

---
+++ AdventOfCode2025/src/App/Program.fs
@@ -0,0 +1,20 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open Days
+open System
+
+// Dispatch table: map numbers to functions
+let dispatch n =
+    match n with
+    | 1 -> Day1.solve 
+    | _ ->
+        // default: print available choices and exit
+        failwithf "Unknown function id: %d." n
+
+[<EntryPoint>]
+let main (argv: string[]) =
+    if argv.Length = 0 then failwith "provide day"
+    else
+        match Int32.TryParse(argv.[0]) with
+        | (true, n) -> dispatch n
+        | _ -> failwith "provide day"
+        0
---
+++ AdventOfCode2025/src/Days/Library.fs
@@ -0,0 +1,40 @@
+﻿namespace Days
+open System.IO
+open System
+
+module Day1 =
+    let input file = File.ReadAllLines file
+    let parseLine (line : string) = 
+        line.Split ' ' 
+        |> Array.filter (String.IsNullOrEmpty >> not) 
+        |> Array.map Int32.Parse
+        |> fun pair -> (pair[0], pair[1])
+    let parseFile file = 
+        input file 
+        |> Array.map parseLine
+
+    let solveImpl1 file =
+        parseFile file
+        |> Array.unzip
+        |> fun (x, y) -> Array.sort x, Array.sort y
+        ||> Array.zip
+        |> Array.sumBy (fun (a,b) -> Math.Abs(a-b))
+
+    let solveImpl2 file =
+        parseFile file
+        |> Array.unzip
+        |> fun (xs, ys) -> 
+            let freqMap = 
+                ys 
+                |> Array.groupBy id 
+                |> Array.map (fun (k, arr) -> k, arr.Length) 
+                |> dict
+            xs |> Array.sumBy (fun x ->
+                let n = if freqMap.ContainsKey(x) then freqMap.[x] else 0
+                x * n)
+
+    let solve =
+        let result1 = solveImpl1 "../Days/data/day01_1.txt"
+        let result2 = solveImpl2 "../Days/data/day01_1.txt"
+        printfn "%d ref(3574690)" result1
+        printfn "%d ref(22565391)" result2
