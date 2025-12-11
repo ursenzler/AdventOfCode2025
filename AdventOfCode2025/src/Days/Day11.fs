c36819cb4e058894d3c5b2964e0a04c9f3f96219
Urs Enzler <urs.enzler@gmail.com>
2025-12-11 18:11:53 +0100

    show off Parallel (slower!!!)

--- AdventOfCode2025/src/Days/Day11.fs
+++ AdventOfCode2025/src/Days/Day11.fs
@@ -1,6 +1,7 @@
 module Days.Day11
 
 open System
+open System.Collections.Concurrent
 open System.IO
 open FSharpx
 
@@ -66,6 +67,44 @@ let solve2 path =
             result
 
     findAllPathsToOut Set.empty devices "svr"
+    
+    
+let solve2parallel path =
+    let devices = path |> parseDevices
+
+    let mutable memo = ConcurrentDictionary<string * bool * bool, int64>()
+
+    let rec findAllPathsToOut (visited: Set<string>) devices device =
+        let current =
+            devices
+            |> List.tryFind (fun d -> d.Name = device)
+            |> Option.getOrFail $"could not find device {device}"
+
+        let dacVisited = visited |> Set.contains "dac"
+        let fftVisited = visited |> Set.contains "fft"
+
+        match memo.TryGetValue((current.Name, dacVisited, fftVisited)) with
+        | true,  r -> r
+        | false, _ ->
+            let result =
+                if visited.Contains current.Name then
+                    0L
+                else
+                    match current.Connections with
+                    | [] -> 0
+                    | [ "out" ] when dacVisited && fftVisited -> 1
+                    | [ "out" ] -> 0L
+                    | connections ->
+                        connections
+                        |> List.toArray
+                        |> Array.map (fun connection -> fun () -> findAllPathsToOut (visited |> Set.add current.Name) devices connection)
+                        |> Array.Parallel.sumBy (fun f -> f())
+
+            memo.TryAdd((current.Name, dacVisited, fftVisited), result) |> ignore
+
+            result
+
+    findAllPathsToOut Set.empty devices "svr"    
 
 
 let solve () =
@@ -75,4 +114,14 @@ let solve () =
     //printfn $"""part 2 example = %A{solve2 pathInput}"""
     let pathInput = Path.Combine (__SOURCE_DIRECTORY__, "Day11Input.txt")
     //printfn $"""part 1 = %A{solve1 pathInput}"""
-    printfn $"""part 2 = %A{solve2 pathInput}"""
+    
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+    
+    let solve2 = solve2 pathInput
+    stopwatch.Stop()
+    printfn $"""part 2 = %A{solve2} in %A{stopwatch.ElapsedMilliseconds}ms"""
+    
+    stopwatch.Restart()
+    let solve2Parallel = solve2parallel pathInput
+    stopwatch.Stop()
+    printfn $"""part 2 parallel = %A{solve2Parallel} in %A{stopwatch.ElapsedMilliseconds}ms"""
